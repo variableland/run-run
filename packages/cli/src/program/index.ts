01a11b4078b344bcc3a11a29aa02de37255f6873
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2025-03-10 19:50:38 -0500

    feat: add version

--- packages/cli/src/program/index.ts
+++ packages/cli/src/program/index.ts
@@ -5,10 +5,12 @@ import { formatCommand } from "./commands/format";
 import { lintCommand } from "./commands/lint";
 import { testStaticCommand } from "./commands/test-static";
 import { typecheckCommand } from "./commands/typecheck";
+import { getVersion } from "./version";
 
 export function createProgram() {
   return new Command("run-run")
     .alias("rr")
+    .version(getVersion(), "-v, --version")
     .addHelpText("before", BANNER_TEXT)
     .addHelpText("after", CREDITS_TEXT)
     .addCommand(formatCommand)
