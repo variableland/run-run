fbd0ad3372f3ad76920aa3b0a5288d358611ee39
rcrd <raquirozbazan@gmail.com>
2025-04-11 23:27:58 -0500

    fix: move to fully ts (#71)

--- packages/run-run/bin.ts
+++ packages/run-run/bin.ts
@@ -1,11 +1,3 @@
 #!/usr/bin/env bun
-// This bin script is intended to be used from the monorepo root directory
-// as well as from the installed /bin directory
 Bun.env.BIN_PATH = __dirname;
-
-if (Bun.env.ENV === "dev") {
-  await import("./src/main");
-} else {
-  // @ts-expect-error - only available after build
-  await import("./dist/main");
-}
+import("./src/main");
